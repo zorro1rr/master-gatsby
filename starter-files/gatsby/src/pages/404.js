35a353bb4498c64761658e95aa1aca22c84b17cb
zorro1rr <zorro1rr@gmail.com>
2020-10-18 21:42:34 -0600

    added Global Styles, but no background img

---
+++ starter-files/gatsby/src/pages/404.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function FourOhFour() {
+  return <p>Hey - That page doesn't exist!!!</p>;
+}
---
+++ starter-files/gatsby/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default function index() {
+  return (
+    <>
+      <p>Hey! I'm the home page.</p>
+    </>
+  );
+}
