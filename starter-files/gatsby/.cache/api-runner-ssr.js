606951ef37719f24d6226cd07be370a4247fe19c
zorro1rr <zorro1rr@gmail.com>
2020-11-13 22:56:34 -0700

    pushing public files

---
+++ starter-files/gatsby/.cache/api-runner-ssr.js
@@ -0,0 +1,55 @@
+var plugins = [{
+      plugin: require('C:/Users/Zorro/Desktop/projects/MasterGatsby/master-gatsby/starter-files/gatsby/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('C:/Users/Zorro/Desktop/projects/MasterGatsby/master-gatsby/starter-files/gatsby/node_modules/gatsby-plugin-styled-components/gatsby-ssr'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('C:/Users/Zorro/Desktop/projects/MasterGatsby/master-gatsby/starter-files/gatsby/node_modules/gatsby-source-sanity/gatsby-ssr'),
+      options: {"plugins":[],"projectId":"8m09jdhk","dataset":"production","watchMode":true},
+    },{
+      plugin: require('C:/Users/Zorro/Desktop/projects/MasterGatsby/master-gatsby/starter-files/gatsby/gatsby-ssr'),
+      options: {"plugins":[]},
+    }]
+// During bootstrap, we write requires at top of this file which looks like:
+// var plugins = [
+//   {
+//     plugin: require("/path/to/plugin1/gatsby-ssr.js"),
+//     options: { ... },
+//   },
+//   {
+//     plugin: require("/path/to/plugin2/gatsby-ssr.js"),
+//     options: { ... },
+//   },
+// ]
+
+const apis = require(`./api-ssr-docs`)
+
+// Run the specified API in any plugins that have implemented it
+module.exports = (api, args, defaultReturn, argTransform) => {
+  if (!apis[api]) {
+    console.log(`This API doesn't exist`, api)
+  }
+
+  // Run each plugin in series.
+  // eslint-disable-next-line no-undef
+  let results = plugins.map(plugin => {
+    if (!plugin.plugin[api]) {
+      return undefined
+    }
+    const result = plugin.plugin[api](args, plugin.options)
+    if (result && argTransform) {
+      args = argTransform({ args, result })
+    }
+    return result
+  })
+
+  // Filter out undefined results.
+  results = results.filter(result => typeof result !== `undefined`)
+
+  if (results.length > 0) {
+    return results
+  } else {
+    return [defaultReturn]
+  }
+}
---
+++ starter-files/gatsby/public/component---src-pages-beer-js-686f4a755a5dd0e77e0b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{D458:function(e,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return a}));var r=t("q1tI"),u=t.n(r);function a(){return u.a.createElement("div",null,u.a.createElement("p",null,"Hey! I'm the Beers page."))}}}]);
+//# sourceMappingURL=component---src-pages-beer-js-686f4a755a5dd0e77e0b.js.map
